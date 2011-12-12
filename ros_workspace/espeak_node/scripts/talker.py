7121d58cb4dfa206bf37b76521dca0869822ecd9
Jonathan Kim <jkimbo@gmail.com>
2011-12-12 17:02:26 +0000

    Modified talker to post to interface topic

--- ros_workspace/espeak_node/scripts/talker.py
+++ ros_workspace/espeak_node/scripts/talker.py
@@ -8,11 +8,14 @@ from std_msgs.msg import String
 
 def talker():
     pub = rospy.Publisher('talk', String)
+    talk = rospy.Publisher('interface', String)
     rospy.init_node('talker')
     while not rospy.is_shutdown():
         string = sys.stdin.readline()
-	pub.publish(String(string))
-	rospy.sleep(1.0)
+        pub.publish(String(string))
+        output = '{"talk": "'+string.rstrip()+'"}'
+        talk.publish(String(output))
+        rospy.sleep(1.0)
 
 if __name__ == '__main__':
     try:
